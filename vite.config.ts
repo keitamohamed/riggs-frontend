912f350e60ca6b5c4872fd3a4b2c06564affd235
mohamedkeita <keitamohamed12@gmail.com>
2023-10-24 10:35:49 -0400

    Remove proxy port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      "/riggs": "https://riggs.up.railway.app",
+      // "/riggs": "https://riggs.up.railway.app",
       // "/riggs": "http://localhost:8080",
     }
   },
