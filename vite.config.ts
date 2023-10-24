3c8f703234958044a6a708e73ebd77cd92e012f5
mohamedkeita <keitamohamed12@gmail.com>
2023-10-24 11:22:47 -0400

    host

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
     proxy: {
-      // "/riggs": "https://riggs.up.railway.app",
+      "/riggs": "https://riggs.up.railway.app",
       // "/riggs": "http://localhost:8080",
     }
   },
